095a7334be4198363592860563938ca775b52b0a
Nick Bottomley <nhbottomley@gmail.com>
2014-07-07 11:14:40 -0700

    Dependency screwing around

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,21 @@
 var extend = require( "extend" );
+var EventEmitter = require( "events" ).EventEmitter;
+var app = Object.create( EventEmitter.prototype );
 
-var app = window.app;
-
-module.exports = ( function( document ) {
+module.exports = ( function( window ) {
   "use strict";
+
   extend( app, {
     currentFile: require( "../js/file.js" ),
     octonode: require( "../js/as-promised.js" )( "octonode" ),
-    nodegit: ( "nodegit" ),
+    git: require( "../js/as-promised.js" )( "gift" ),
     githubClient: null
   });
 
+  if ( window.app == null ) {
+    window.app = app;
+  }
+
   return app;
 
-})( window.document );
+})( window );
